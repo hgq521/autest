935a9d1d246fc49a2b4e72b86523ac9d4dc88620
shuiniu <h.eg1@163.com>
2019-10-11 15:34:51 +0800

    增加rethinkdb操作模块

---
+++ db/config.py
@@ -0,0 +1,6 @@
+class cfg:
+	def __init__(self):
+		self.ip = '192.168.14.111'
+		self.port = 28015
+		self.db = "autest"
+		
---
+++ db/myrethinkdb.py
@@ -0,0 +1,38 @@
+from .config import cfg
+from rethinkdb import RethinkDB
+r = RethinkDB()
+
+class mydb:
+	def __init__(self):
+		self.cfg = cfg()
+		self.conn = {}
+		self.db = {}
+		
+		pass
+
+	def connect(self):
+		self.conn = r.connect(self.cfg.ip, self.cfg.port)
+		self.conn.repl()
+		pass
+
+	def create_db(self, db_name):
+		if db_name not in r.db_list().run():
+			r.db_create(db_name).run()
+		pass
+
+	def create_tab(self, tab_name):
+		db = self.db
+		if tab_name not in db.table_list().run():
+			db.table_create(tab_name).run()
+		pass
+
+	def get_db(self, db_name):
+		self.create_db(db_name)
+		self.db = r.db(db_name)
+		return self.db
+
+	def tab(self, tab_name):
+		self.create_tab(tab_name)
+		return self.db.table(tab_name)
+
+
