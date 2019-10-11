6f0634d004cd97fd99ba6ca4e62b4a9f7a52c477
shuiniu <h.eg1@163.com>
2019-10-11 18:18:26 +0800

    mytime 增加hour接口 获得整点秒数

--- mytime/mytime.py
+++ mytime/mytime.py
@@ -10,13 +10,20 @@ def today_hour(hour): #24小时制
 	sec = time.time() 
 	return (sec - sec % day_hours() + hour * hour_sec() - 8 * hour_sec())
 
+def hour(sec):
+	tmp = time.localtime(sec)
+
+	#return sec - tmp.tm_min * 60 - tmp.tm_sec
+	return sec - sec % hour_sec()
 def wday(sec):
 	tt = time.localtime(sec)
 	return (tt.tm_wday+1)
 
 if __name__ == '__main__':
 	sec = today_hour(5)
+	sec = time.time()
 	print(sec)
+	sec = hour(sec)
 	print(time.localtime(sec))
 	print(wday(sec))
 
